f6b26dd85e9fd27c2ef72a392aeddc04386b0263
matias-burgos <matiasburgos_98@hotmail.com>
2017-05-15 18:09:24 -0300

    conserje

--- src/Paquete/Pasajero.java
+++ src/Paquete/Pasajero.java
@@ -1,10 +1,14 @@
 package Paquete;
 
+import java.io.FileOutputStream;
+
 public class Pasajero extends Persona
 {
 	String ciudadOrigen;//Ciudad de origen.
 	String domicilioOrigen;//Domicilio en el que vive.
 	//Hacer metodo para almacenar historial de pasajero en el hotel.
+	FileOutputStream historial=new FileOutputStream("Historial.sd");//Posible opcion para almacenar el historial de cada pasajero.
+	
 	
 	public Pasajero()
 	{
