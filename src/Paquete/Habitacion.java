4754747d843bae90bf7da17813f9b5600fbb0036
matias-burgos <matiasburgos_98@hotmail.com>
2017-05-18 17:33:06 -0300

    Habitacion

---
+++ src/Paquete/Habitacion.java
@@ -0,0 +1,53 @@
+package Paquete;
+
+public class Habitacion {
+		boolean ocupada;//Si la habitacion se encuentra ocupada (true) o disponible (false)
+		
+		//Posibles atributos:
+		int periodoOcupacion;//Es el periodo por el que la habitacion esta ocupada.
+		//Datos del cliente que alquila la habitacion.
+		String nombre;
+		int dni;
+		String ciudadOrigen;
+		String domicilioOrigen;
+		
+		public Habitacion()
+		{
+			ocupada=true;
+			
+			//
+			periodoOcupacion=0;
+			nombre="";
+			dni=0;
+			ciudadOrigen="";
+			domicilioOrigen="";
+		}
+		
+		public int getPeriodo()
+		{
+			return periodoOcupacion;
+		}
+		public String getNombreClient()
+		{
+			return nombre;
+		}
+		
+		public int getDNI()
+		{
+			return dni;
+		}
+		
+		public String getCiudadClient()
+		{
+			return ciudadOrigen;
+		}
+		public String getDomicilioClient()
+		{
+			return domicilioOrigen;
+		}
+		public boolean getCupada()
+		{
+			return ocupada;
+		}
+		
+}
