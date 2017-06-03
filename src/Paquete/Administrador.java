f7ba1af39e765eb7acc836a2e7e24d4e19de52e0
LucasPollo <lucasfalaschini@gmail.com>
2017-06-03 14:13:09 -0300

    Aumento y disminución de permisos
    También se le agrego la variable permisos al conserje con sus respectivos get y set.

--- src/Paquete/Administrador.java
+++ src/Paquete/Administrador.java
@@ -1,3 +1,11 @@
+/*
+ * Administrador: Es el encargado de las funciones administrativas del sistema.
+ *  Dentro de sus funciones está la realización de back ups de la información, 
+ *  la creación de otros usuarios, la asignación de permisos a usuarios, etc.
+ * 
+ * */
+ 
+
 package Paquete;
 
 import java.io.FileInputStream;
@@ -20,6 +28,39 @@ public class Administrador extends UserHotel{
 		System.out.println("Tipo de usuario: Administrador");
 		MostrarUsuario();
 	}
+	
+	
+	
+	// Aumenta el nivel de permisos del conserje. Si ya tiene el maximo tira un mensaje
+	public void aumentarPermisos(Conserje conserje)
+	{
+		if(conserje.getPermisos()==3)
+		{
+			System.out.println("El conserje ya tiene el nivel maximo de permisos");
+		}
+		else
+		{
+			int aux = conserje.getPermisos();
+			conserje.setPermisos(aux+1);
+			System.out.println("Permisos aumentados");
+		}
+	}
+	
+	// Quita un nivel de permisos al conserje. Si ya tiene el nivel minimo, da un mensaje
+	public void quitarPermisos(Conserje conserje)
+	{
+		if(conserje.getPermisos()==1)
+		{
+			System.out.println("El conserje ya tiene el nivel minimo de permisos");
+		}
+		else
+		{
+			int aux = conserje.getPermisos();
+			conserje.setPermisos(aux-1);
+			System.out.println("Permisos quitados");
+		}
+	}
+	
 	
 	//Ejemplo.
 	/*
--- src/Paquete/Conserje.java
+++ src/Paquete/Conserje.java
@@ -1,5 +1,16 @@
 package Paquete;
 
-public class Conserje extends UserHotel{
+public class Conserje extends UserHotel
+{
+		int permiso;
 		
+		public int getPermisos()
+		{
+			return permiso;
+		}
+		
+		public void setPermisos(int permiso)
+		{
+			this.permiso = permiso;
+		}
 }
