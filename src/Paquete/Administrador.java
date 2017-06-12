3ce7225e3cce7332fa0c3b7041cd7f433f007560
LucasPollo <lucasfalaschini@gmail.com>
2017-06-12 00:13:54 -0300

    asd
    asd

--- src/Paquete/Administrador.java
+++ src/Paquete/Administrador.java
@@ -10,9 +10,14 @@ package Paquete;
 
 import java.util.Scanner;
 
+
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Administrador extends UserHotel implements Serializable {
@@ -68,6 +73,33 @@ public class Administrador extends UserHotel implements Serializable {
 	public int cantidadEmpleados() 
 	{
 		int cant=0;
+		FileInputStream empleados;
+		try
+		{
+			empleados = new FileInputStream("Empleados.dat");
+			ObjectInputStream lectura = new ObjectInputStream(empleados);
+			Conserje aux=(Conserje)lectura.readObject();
+						
+			while(aux != null)
+			{
+				aux=(Conserje)lectura.readObject();
+				cant++;	
+			}	
+			lectura.close();
+		}	
+		
+		catch(NullPointerException ex)
+		{
+			System.out.println("El archivo esta vacio: "+ex);
+		}
+		catch(IOException ex)
+		{
+			System.out.println("No se pudo leer el archivo: "+ex);
+		}
+		catch(ClassNotFoundException ex)
+		{
+			System.out.println("No se pudo leer el archivo: "+ex);
+		}
 		
 		return cant;
 	}
@@ -84,6 +116,7 @@ public class Administrador extends UserHotel implements Serializable {
 			Conserje aux=(Conserje)lectura.readObject();
 			System.out.println("Nombre: " + aux.getNombre());
 			System.out.println("DNI: " + aux.getDNI());
+			lectura.close();
 			
 		}
 		
@@ -124,7 +157,7 @@ public class Administrador extends UserHotel implements Serializable {
 				System.out.println("DNI: " + aux.getDNI());	
 			}
 			
-			
+			lectura.close();
 		}
 		
 		
@@ -140,9 +173,16 @@ public class Administrador extends UserHotel implements Serializable {
 		{
 			System.out.println("No se pudo leer el archivo: "+ex);
 		}
+		
+		
 	}
 	
 	
+	
+	
+	
+	
+	/*
 	//Pide al usuario los datos necesarios para crear un objeto de tipo conserje
 	public Conserje crearConserje()
 	{
@@ -176,20 +216,76 @@ public class Administrador extends UserHotel implements Serializable {
 		
 		return conserje;
 	}
+	*/
 	
-	
-	//Agrega un empleado al archivo de empleados.
-	public void agregarEmpleado()
+	//Crea y a su vez agrega un empleado al archivo de empleados.
+	public void crearEmpleado(String nombre,int dni, String usuario, String pass, String id)
 	{
+		
+		Conserje nuevo = new Conserje (nombre,dni,usuario,pass,id);
+		
+		
+		FileOutputStream empleados;
+		
+		try
+		{
+			empleados=new FileOutputStream("empleados.dat");
+			ObjectOutputStream escritura=new ObjectOutputStream(empleados);
+			escritura.writeObject(nuevo);
+			escritura.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			System.out.println("No se pudo crear el archivo: "+ex);
+		}
+		catch(IOException ex)
+		{
+			System.out.println("No se pudo crear el archivo:"+ex);
+		}
 		
 	}
 	
 	
-	//Recorre un archivo de empleados y elimina un empleado buscandolo por su nombre y apellido
-	public void eliminarEmpleado()
+	//Recorre un archivo de empleados y elimina un empleado buscandolo por su ID
+	//Este metodo recorre el archivo y vuelve null al objeto que tenga la misma id 
+	//que la pasada por parametro. En caso de que no sirva, hacer un metodo que cree un nuevo
+	//archivo con lo mismo pero sin el que se quiera eliminar
+	public void eliminarEmpleado(String id)
 	{
+		FileInputStream empleados;
+		try
+		{
+			empleados = new FileInputStream("Empleados.dat");
+			ObjectInputStream lectura = new ObjectInputStream(empleados);
+			Conserje aux=(Conserje)lectura.readObject();
+						
+			while(aux != null)
+			{
+				aux=(Conserje)lectura.readObject();
+				
+				if(aux.getEmpleadoID()==id)
+				{
+					aux = null;
+				}			
+			}
+			lectura.close();
+		}
 		
+		catch(NullPointerException ex)
+		{
+			System.out.println("El archivo esta vacio: "+ex);
+		}
+		catch(IOException ex)
+		{
+			System.out.println("No se pudo leer el archivo: "+ex);
+		}
+		catch(ClassNotFoundException ex)
+		{
+			System.out.println("No se pudo leer el archivo: "+ex);
+		}
 	}
+	
+	
 	
 	
 	//Recorre un archivo de habitaciones y la elmina segun el numero de habitacion
